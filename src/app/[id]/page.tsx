1a22cc052e06adda0b982399fbafa7bb0ace7c9c
arif2758 <arif2758@gmail.com>
2023-12-09 14:14:26 +0600

    second commit

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -2,24 +2,24 @@
 import EditTopicForm from "@/components/EditTopicForm";
 import React from "react";
 
-// const getTopicById = async (id) => {
-//   const apiUrl = process.env.API_URL
-//   try {
-//     const res = await fetch(`${apiUrl}/api/topics/${id}`, {
-//       cache: "no-store",
-//     });
-//     if (!res.ok) {
-//       throw new Error("Failed to fetch topic");
-//     }
-//     return res.json();
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+const getTopicById = async (id:string) => {
+  const apiUrl = process.env.API_URL
+  try {
+    const res = await fetch(`${apiUrl}/api/topics/${id}`, {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to fetch topic");
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-// export default async function EditTopic({ params }) {
-//   const { id } = params;
-//   const { topic } = await getTopicById(id);
-//   const { title, description } = topic;
-//   return <EditTopicForm id={id} title={title} description={description} />;
-// }
+export default async function EditTopic({ params }: { params: { id: string } }) {
+  const { id } = params;
+  const { topic } = await getTopicById(id);
+  const { title, description } = topic;
+  return <EditTopicForm id={id} title={title} description={description} />;
+}
--- src/components/EditTopicForm.tsx
+++ src/components/EditTopicForm.tsx
@@ -2,52 +2,52 @@
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-export default function EditTopicForm() {
-  // const router = useRouter();
-  // const [newTitle, setNewTitle] = useState(title);
-  // const [newDescription, setNewDescription] = useState(description);
+export default function EditTopicForm({id, title, description}:{id: string, title: string, description:string}) {
+  const router = useRouter();
+  const [newTitle, setNewTitle] = useState(title);
+  const [newDescription, setNewDescription] = useState(description);
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
+  const handleSubmit = async (e:any) => {
+    e.preventDefault();
 
-  //   try {
-  //     const res = await fetch(`/api/topics/${id}`, {
-  //       method: "PUT",
-  //       headers: { "Content-type": "application/json" },
-  //       body: JSON.stringify({ newTitle, newDescription }),
-  //     });
-  //     if (!res.ok) {
-  //       throw new Error("Failed to update topic");
-  //     }
-  //     router.refresh();
-  //     router.push("/");
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+    try {
+      const res = await fetch(`/api/topics/${id}`, {
+        method: "PUT",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ newTitle, newDescription }),
+      });
+      if (!res.ok) {
+        throw new Error("Failed to update topic");
+      }
+      router.refresh();
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  // return (
-  //   <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-  //     <input
-  //       value={newTitle}
-  //       onChange={(e) => setNewTitle(e.target.value)}
-  //       type="text"
-  //       placeholder="Topic Title"
-  //       className="border border-slate-500 px-8 py-2"
-  //     />
-  //     <input
-  //       value={newDescription}
-  //       onChange={(e) => setNewDescription(e.target.value)}
-  //       type="text"
-  //       placeholder="Topic Description"
-  //       className="border border-slate-500 px-8 py-2"
-  //     />
-  //     <button
-  //       type="submit"
-  //       className="bg-green-600 font-bold text-white py-3 px-6 w-fit"
-  //     >
-  //       Update Topic
-  //     </button>
-  //   </form>
-  // );
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+      <input
+        value={newTitle}
+        onChange={(e) => setNewTitle(e.target.value)}
+        type="text"
+        placeholder="Topic Title"
+        className="border border-slate-500 px-8 py-2"
+      />
+      <input
+        value={newDescription}
+        onChange={(e) => setNewDescription(e.target.value)}
+        type="text"
+        placeholder="Topic Description"
+        className="border border-slate-500 px-8 py-2"
+      />
+      <button
+        type="submit"
+        className="bg-green-600 font-bold text-white py-3 px-6 w-fit"
+      >
+        Update Topic
+      </button>
+    </form>
+  );
 }
